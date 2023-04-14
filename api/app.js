d4bf05d5a7497ce6c34bef02a40b25e48a89754f
Luis David Lopez Zapata <luisdavidlz0202@gmail.com>
2023-04-14 01:50:24 -0500

    config files refactor

--- api/app.js
+++ api/app.js
@@ -1,17 +1,25 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+import env from './connection/config.js'
+
 import route from './routes/routes.js'
 
 const app = express();
 
+app.set('port', env.PORT)
+
+app.use(express.json())
+app.use(morgan('dev'))
 app.use(cors());
-app.use(bodyParser.urlencoded({
-    limit: "50mb",
-    extended: false
-}));
-app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use('/api', route);
+
+app.use((req, res) => {
+    res.status(404).json({
+        error: 'Route Not Found'
+    })
+})
 
 export default app;
--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 import { conectionSequelize } from "./api/connection/db.js";
 import app from "./api/app.js";
 
-app.set("port", process.env.PORT_SERVER || 3000);
-
 const main = async () => {
   try {
-    await conectionSequelize.sync({ force: false });
-
-    app.listen(app.get("port"), () => {
-      console.log(`Server running in port ${app.get("port")}`);
-    });
+    await conectionSequelize.sync({ force: false })
+    app.listen(app.get('port'), () => {
+      console.log(`🆗✅🆗 Server on port ${app.get('port')} 🆗✅🆗`)
+    })
   } catch (err) {
-    console.error(`Not connection database ${err}`);
+    console.error(`🛑⛔ Not connection database ❗${err}❗ ⛔🛑`)
   }
-};
+}
 
 main();
