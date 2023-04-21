7d06c7c82989827b14238f8497802603b15bf1dc
Luis David Lopez Zapata <luisdavidlz0202@gmail.com>
2023-04-21 10:39:18 -0500

    movies route fixed

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -7,7 +7,7 @@ import genreController from '../controllers/genreController.js'
 const route = Router();
 //---- Movies module
 route.post('/movies/admin', movieController.create_movie);
-route.get('/movies/admin',movieController.get_movies);
+route.get('/movies',movieController.get_movies);
 route.get('/movies/:movieId', movieController.get_movie_by_id);
 route.put('/movies/admin/:movieId', movieController.update_movie_by_id);
 route.delete('/movies/admin/:movieId', movieController.delete_movie_by_id);
